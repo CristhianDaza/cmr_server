399a931ed57b526ecdaa0fac1723829cb76bf853
Cristhian Daza <cristian.d.b@hotmail.com>
2020-07-28 20:57:41 -0500

    Agregando Schema de Cliente

---
+++ schema.js
@@ -0,0 +1,16 @@
+import { buildSchema } from 'graphql';
+const schema = buildSchema(`
+  type Cliente {
+    id: ID
+    nombre: String
+    apellido: String
+    empresa: String
+    email: String
+  }
+
+  type Query {
+    cliente: Cliente
+  }
+`);
+
+export default schema;
