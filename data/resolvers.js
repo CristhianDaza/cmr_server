bea6b0b50f7e9aa1792a48d43e0701d34c208ebd
Cristhian Daza <cristian.d.b@hotmail.com>
2020-07-29 21:03:37 -0500

    Modificando query para traer la cantidad total de clientes

--- data/resolvers.js
+++ data/resolvers.js
@@ -3,8 +3,8 @@ import { Clientes } from './db';
 
 export const resolvers = {
   Query: {
-    getClientes: (root, { limite }) => {
-      return Clientes.find({}).limit(limite)
+    getClientes: (root, { limite, offset }) => {
+      return Clientes.find({}).limit(limite).skip(offset)
     },
     getCliente:(root, { id }) => {
       return new Promise((resolve, object) => {
@@ -13,6 +13,14 @@ export const resolvers = {
           else resolve(cliente)
         })
       });
+    },
+    totalClientes: (root) => {
+      return new Promise((resolve, object) => {
+        Clientes.countDocuments({}, (error, count) => {
+          if(error) rejects(error)
+          else resolve(count)
+        })
+      })
     }
   },
   Mutation: {
