934f55d2c38fbe2257a8ab10cab0bc728750f2d7
Cristhian Daza <cristian.d.b@hotmail.com>
2020-08-09 20:05:15 -0500

    creando resolve de nuevo pedido

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { Clientes, Productos } from './db';
+import { Clientes, Productos, Pedidos } from './db';
 
 export const resolvers = {
   Query: {
@@ -110,5 +110,21 @@ export const resolvers = {
         })
       });
     },
+    nuevoPedido: (root, { input }) => {
+      const nuevoPedido = new Pedidos({
+        pedido: input.pedido,
+        total: input.total,
+        fecha: new Date(),
+        cliente: input.cliente,
+        estado: 'PENDIENTE'
+      });
+      nuevoPedido.id = nuevoPedido._id;
+      return new Promise((resolve, object) => {
+        nuevoPedido.save((error) => {
+          if(error) rejects(error)
+          else resolve(nuevoPedido)
+        })
+      })
+    }
   }
 }
